f815c6ebb9754b4995709de271909dae7a01567a
Tillo <tillo.bosshart@gmail.com>
2019-12-10 15:27:13 +0100

    Added ASCII art

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/morgulbrut/color256"
+
 	"github.com/akamensky/argparse"
 	"github.com/morgulbrut/colorlog"
 	"github.com/morgulbrut/wpCollector/provider/wallhaven"
@@ -12,9 +14,10 @@ import (
 )
 
 func main() {
+	logo()
 	pr := argparse.NewParser("wallpaperscraper", "downloads a bunch of wallpapers from different providers")
-	query := pr.String("q", "query", &argparse.Options{Required: true, Help: "Keyword"})
-	size := pr.String("s", "size", &argparse.Options{Required: true, Help: "Size, format 1920x1080"})
+	query := pr.String("q", "query", &argparse.Options{Required: true, Help: "Keyword (required)"})
+	size := pr.String("s", "size", &argparse.Options{Required: true, Help: "Size, format 1920x1080 (required)"})
 	wpp := pr.Flag("1", "wallpaperplay", &argparse.Options{Required: false, Help: "Download from wallpaperplay"})
 	wps := pr.Flag("2", "wallpapersite", &argparse.Options{Required: false, Help: "Download from wallpapersite"})
 	wh := pr.Flag("3", "wallhaven", &argparse.Options{Required: false, Help: "Download from wallhaven"})
@@ -35,4 +38,16 @@ func main() {
 		colorlog.Info("Scraping wallhaven...")
 		wallhaven.Get(*query, *size, 10)
 	}
+}
+
+func logo() {
+	logo := `
+               _________        .__  .__                __               
+__  _  _______ \_   ___ \  ____ |  | |  |   ____  _____/  |_ ___________ 
+\ \/ \/ \____ \/    \  \/ /  _ \|  | |  | _/ __ _/ ___\   __/  _ \_  __ \
+ \     /|  |_> \     \___(  <_> |  |_|  |_\  ___\  \___|  |(  <_> |  | \/
+  \/\_/ |   __/ \______  /\____/|____|____/\___  \___  |__| \____/|__|   
+		|__|           \/                      \/    \/                 `
+
+	color256.PrintRed(logo)
 }
